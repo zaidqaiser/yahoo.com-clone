e6904772471779092f231b84c8f30fa2a0f81bdf
Creative Dev <zaidqaiser152@gmail.com>
2024-07-19 23:51:12 +0500

    yahoo.com-clone

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Public Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
